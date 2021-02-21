3eb85e162d7592fb74194db96558640e47920926
Eddie Chu <chu08888@gmail.com>
2021-02-20 23:22:33 -0800

    use netlify forms

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,8 +1,19 @@
 import React from "react"
+import Nform from "../Nform"
+
 const Footer = () => {
   return (
-    <footer class="footer">
-      <div class="footer-background-container-right mobile-hide"></div>
+    <footer className="footer">
+      <div className="footer-background-container-right mobile-hide"></div>
+      <div className="footer-container flex-block">
+        <div className="footer-container-left">
+          <h2 className="footer-title">Get in touch!</h2>
+          <p className="footer-text">
+            Send us a message and well get back to you as soon as possible!
+          </p>
+        </div>
+        <Nform />
+      </div>
     </footer>
   )
 }
---
+++ src/components/Nform.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react"
+import { navigate } from "gatsby-link"
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+export default class Nform extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { isValidated: false }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <>
+        <form
+          className="n-form"
+          name="contact"
+          method="post"
+          action="/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <div hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="field">
+            <label className="label" htmlFor={"name"}>
+              Your Name
+            </label>
+            <div className="control">
+              <input
+                className="input"
+                type={"text"}
+                name={"name"}
+                onChange={this.handleChange}
+                id={"name"}
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label" htmlFor={"email"}>
+              Email
+            </label>
+            <div className="control">
+              <input
+                className="input"
+                type={"email"}
+                name={"email"}
+                onChange={this.handleChange}
+                id={"email"}
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label" htmlFor={"message"}>
+              Message
+            </label>
+            <div className="control">
+              <textarea
+                className="textarea"
+                name={"message"}
+                onChange={this.handleChange}
+                id={"message"}
+                required={true}
+                placeholder="Message"
+              />
+            </div>
+          </div>
+          <div className="field text-right mb-0">
+            <button className="btn btn--orange" type="submit">
+              Send Message
+            </button>
+          </div>
+        </form>
+      </>
+    )
+  }
+}
